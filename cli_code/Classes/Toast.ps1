ac0c980676a524e798b2114523ea5b0af59ea6b3
Klüpfel Annika <AnnikaKluepfel@web.de>
2025-09-23 16:42:58 +0200

    add simple toast using Windows.UI

--- cli_code/Classes/Toast.ps1
+++ cli_code/Classes/Toast.ps1
@@ -8,6 +8,11 @@ class Toast {
             'info'  { $icon = $this.InfoIcon }
             'error' { $icon = $this.ErrorIcon }
         }
-        New-BurntToastNotification -Text "$icon $Title", $Message
+        $ToastText02 = [Windows.UI.Notifications.ToastTemplateType, Windows.UI.Notifications, ContentType = WindowsRuntime]::ToastText02
+        $TemplateContent = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::GetTemplateContent($ToastText02)
+        $TemplateContent.SelectSingleNode("//text[@id='1']").InnerText = "BGH-CLI | $icon $Title"
+        $TemplateContent.SelectSingleNode("//text[@id='2']").InnerText = $Message
+        $AppId = "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe"
+        [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($AppId).Show($TemplateContent)
     }
 }
