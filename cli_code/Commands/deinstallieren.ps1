48d1df30640f5ded1f749c46ae65f3eaf11c4b23
Klüpfel Annika <AnnikaKluepfel@web.de>
2025-10-01 11:37:36 +0200

    add command 'deinstallieren'

---
+++ cli_code/Commands/deinstallieren.ps1
@@ -0,0 +1,18 @@
+try {
+    Out-Message "ACHTUNG: Dies kann nicht rückgängig gemacht werden. Um das CLI danach wieder zu nutzen, musst du es erneut installieren." -Type "warning"
+    do {
+        $confirmUninstall = Read-Host "Deinstallieren? (j, sonst beenden mit Enter)"
+    } while ($confirmUninstall -ne "j" -and $confirmUninstall -ne "")
+    if ($confirmUninstall -eq "") {
+        Out-Message "Deinstallation abgebrochen."
+        return
+    }
+    $TempUpdateFilePath = Invoke-UninstallBootstrap
+    Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -NoExit -File `"$TempUpdateFilePath`" -InstallPath `"$PWD`""
+    Out-Message "Deinstallation vorbereitet."
+    exit 0
+} catch {
+    Out-Message $_.Exception.Message -Type "error"
+    Write-ErrorMessage -Log $log -ErrMsg $_.Exception.Message
+    Send-ErrorReport -ErrMsg $_.Exception.Message
+}
