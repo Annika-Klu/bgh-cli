6547e9ffc568a5ee205477c1203ef10eb6512060
Klüpfel Annika <AnnikaKluepfel@web.de>
2025-10-22 12:24:07 +0200

    add 'Dates' module with method for quarter start/end dates

---
+++ cli_code/Modules/Dates.psm1
@@ -0,0 +1,49 @@
+function Get-StartEndDates {
+    param(
+        [Int]$Quarter,
+        [Int]$Year
+    )
+
+    $heute = Get-Date
+    $definitionMode = "angegebenes"
+    if (-not $Year) { $Year = $heute.Year }
+    if (-not $Quarter) {
+        $definitionMode = "kommendes" 
+        $currentQuarter = [math]::Ceiling($heute.Month / 3)
+        $Quarter = $currentQuarter + 1
+
+        if ($Quarter -gt 4) {
+            $Quarter = 1
+            $Year += 1
+        }
+    }
+
+    Out-Message "Erstelle Plan für $definitionMode Q$Quarter-$Year..."
+
+    switch ($Quarter) {
+        1 {
+            $startMonth = 1
+            break
+        }
+        2 {
+            $startMonth = 4
+            break
+        }
+        3 {
+            $startMonth = 7
+            break
+        }
+        4 {
+            $startMonth = 10
+            break
+        }
+        default {
+            Throw "Ungültiges Quartal: $Quarter"
+        }
+    }
+
+    $start = Get-Date -Year $Year -Month $startMonth -Day 1
+    $end = $start.AddMonths(3).AddDays(-1)
+
+    return @($start, $end)
+}
