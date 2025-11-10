76822bc3e7a3e840979e54d75be5388b7668eaa1
Klüpfel Annika <AnnikaKluepfel@web.de>
2025-11-10 22:32:09 +0100

    add methods for wikis

---
+++ cli_code/Modules/churchtools/Wikis.psm1
@@ -0,0 +1,41 @@
+function Get-Wikis {
+    $ct = [ChurchTools]::new($CT_API_URL)
+    return $ct.PaginateRequest("wiki/categories", 100)
+}
+
+function Get-WikiPages {
+    param(
+        [int]$WikiCategoryId
+    )
+    $ct = [ChurchTools]::new($CT_API_URL)
+    return $ct.PaginateRequest("wiki/categories/$WikiCategoryId/pages", 100)
+}
+
+function Save-WikiPage {
+    param(
+        [int]$WikiCategoryId,
+        [guid]$WikiPageId,
+        [string]$SavePath
+    )
+    $ct = [ChurchTools]::new($CT_API_URL)
+    $content = $ct.CallApi("GET", "wiki/categories/$WikiCategoryId/pages/$WikiPageId", $null, $null)
+    Set-Content -Path $SavePath -Value $content.data.text
+}
+
+function Save-WikiPageFiles {
+    param(
+        [int]$WikiCategoryId,
+        [guid]$WikiPageId,
+        [string]$SaveDir
+    )
+
+    $ct = [ChurchTools]::new($CT_API_URL)
+    $files = $ct.CallApi("GET", "files/wiki_$WikiCategoryId/$WikiPageId", $null, $null)
+    if ($files.Count -eq 0) {
+        return
+    }
+    foreach ($file in $files.data) {
+        $savePath = Join-Path $SaveDir $file.name
+        $ct.CallApi("GET", $file.fileUrl, $null, $savePath)
+    }
+}
