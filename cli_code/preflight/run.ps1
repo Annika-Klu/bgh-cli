e0b08b5a0fd428ade58f281e68573b802196673d
Klüpfel Annika <AnnikaKluepfel@web.de>
2026-02-14 23:38:36 +0100

    fixes for recognition of init[system] cmd

--- cli_code/preflight/run.ps1
+++ cli_code/preflight/run.ps1
@@ -65,7 +65,7 @@ function Write-PreflightError {
 $initFile = Join-Path $PWD "init"
 
 try {
-    if ((Test-Path $initFile) -or ($Command -eq "init")) {
+    if ((Test-Path $initFile) -or ($Command -eq "init[system]")) {
         Get-DotEnv
         Set-CliEnv
         Remove-Item $initFile -ErrorAction SilentlyContinue
