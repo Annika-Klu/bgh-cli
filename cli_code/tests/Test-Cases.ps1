04a9137af1168f41a31c4eea59f6eba38782429b
Klüpfel Annika <AnnikaKluepfel@web.de>
2026-02-14 19:27:54 +0100

    basic files to run tests from 'cases' sub dir and checking err code

---
+++ cli_code/tests/Test-Cases.ps1
@@ -0,0 +1,57 @@
+﻿function Test-Cases {
+    param(
+        [Parameter(Mandatory)]
+        [Array]$Cases,
+
+        [Parameter(Mandatory)]
+        [string]$CommandName
+    )
+
+    $mainCommand = "bgh"
+    $passed = 0
+    $failed = 0
+
+    foreach ($testCase in $Cases) {
+
+        $arguments = @()
+        $expectedExitCode = if ($testCase.ContainsKey("ExpectedExitCode")) { $testCase.ExpectedExitCode } else { 0 }
+
+        if ($testCase.ContainsKey("Arguments") -and $testCase.Arguments) {
+            foreach ($key in $testCase.Arguments.Keys) {
+                $value = $testCase.Arguments[$key]
+                if ($value -is [bool] -and $value) {
+                    $arguments += "-$key"
+                } else {
+                    $arguments += "-$key"
+                    $arguments += $value
+                }
+            }
+        }
+
+        $testName = if ($testCase.ContainsKey("Name")) { $testCase.Name } else { "(unnamed test)" }
+        Write-Host ("Running test '{0}' for command {1}" -f $testName, $CommandName) -ForegroundColor Gray
+
+        try {
+            # no console outputs
+            & "$PSScriptRoot\..\$mainCommand.ps1" $CommandName @arguments *>$null 2>&1
+
+            # only suppress stout, errs visible:
+            # & "$PSScriptRoot\..\bgh.ps1" $CommandName @arguments >$null
+            
+            $actualExitCode = $LASTEXITCODE
+
+            if ($actualExitCode -ne $expectedExitCode) {
+                Write-Host ("Fehler: erwarteter ExitCode {0}, tatsächlich {1}" -f $expectedExitCode, $actualExitCode) -ForegroundColor Red
+                $failed++
+            } else {
+                Write-Host "Test erfolgreich" -ForegroundColor Green
+                $passed++
+            }
+
+        } catch {
+            Write-Host ("Exception beim Test {0}: {1}" -f $testName, $_) -ForegroundColor Red
+            $failed++
+        }
+    }
+   return ($passed, $failed)
+}
---
+++ cli_code/tests/run.ps1
@@ -0,0 +1,41 @@
+﻿param(
+    [string]$TestFilesRoot = "$PSScriptRoot/cases"
+)
+
+. "$PSScriptRoot/Test-Cases.ps1"
+
+$originalLocation = Get-Location
+$testFiles = Get-ChildItem -Path $TestFilesRoot -Recurse -Filter *.tests.ps1 | Sort-Object Name
+
+if (-not $testFiles) {
+    Write-Host "No tests found" -ForegroundColor Yellow
+    exit 0
+}
+
+$passedTotal = 0
+$failedTotal = 0
+
+foreach ($file in $testFiles) {
+    $parts = ($file.BaseName -split "\.")
+    $commandName = $parts[1]
+
+
+    try {
+        $testCases = . $file.FullName
+        Write-Host ("Running {0} tests for {1}" -f $testCases.Count, $file.Name) -ForegroundColor Cyan
+        ($passed, $failed) = Test-Cases -Cases $testCases -CommandName $commandName
+        Write-Host ""
+        Write-Host ("Finished tests for {0}. Passed: {1}, Failed: {2}" -f $file.Name, $passed, $failed) -ForegroundColor Cyan
+        
+        $passedTotal += $passed
+        $failedTotal += $failed
+
+    } catch {
+        Write-Host ("Exception while running test file {0}: {1}" -f $file.Name, $_) -ForegroundColor Red
+    }
+}
+
+Set-Location $originalLocation
+
+$toalTestsRun = $passedTotal + $failedTotal
+Write-Host "$toalTestsRun tests completed. Passed $passedTotal, failed $failedTotal" -ForegroundColor Cyan
