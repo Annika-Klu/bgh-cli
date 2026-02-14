b89897578c490d1c366eee5f6e072082fccc0c8b
Klüpfel Annika <AnnikaKluepfel@web.de>
2026-02-14 12:38:43 +0100

    assert elevated script run generally/before loading modules

--- cli_code/preflight/installRequirements.ps1
+++ cli_code/preflight/installRequirements.ps1
@@ -3,24 +3,21 @@ $ErrorActionPreference = "Stop"
 $requirements = Import-PowerShellDataFile -Path "$PWD/requirements.psd1"
 
 function Assert-ElevatedScriptRun {
-    param(
-        [string]$ModuleName
-    )
     $currentUser = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
     $isAdmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
     
     if (-not $isAdmin) {
-        throw [System.Exception] "BGH-CLI muss das Modul '$ModuleName' installieren. Dazu bitte PowerShell als Administrator ausführen und Befehl erneut eingeben."
+        Write-Error "Bitte PowerShell als Administrator starten und den Befehl erneut ausführen." -ErrorAction Stop
     }
 }
 
+Assert-ElevatedScriptRun
 foreach ($mod in $requirements.RequiredModules) {
     $name = $mod.Name
     $minVersion = $mod.MinimumVersion
 
     $installed = Get-Module -ListAvailable -Name $name | Where-Object { $_.Version -ge [Version]$minVersion }
     if (-not $installed) {
-        Assert-ElevatedScriptRun -ModuleName $name
         Out-Message "Installiere Modul $name ($($mod.Description))..."
         Install-Module -Name $name -MinimumVersion $minVersion -Force -Scope CurrentUser
     }
